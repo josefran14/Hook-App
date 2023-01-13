fa16a4934557024c7acf0171ac96c7db90982ea1
josefran14 <fransaavedra21@gmail.com>
2023-01-13 00:40:26 -0400

    Cada lista de tarea que se añañadio al estado a traves del input quedo guardada dentro del localStorage, y asi la informacion es persistente

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,24 +1,32 @@
-import { useReducer} from "react";
+import { useEffect, useReducer} from "react";
 import { TodoAdd } from "./TodoAdd";
 import { TodoList } from "./TodoList";
 import { todoReducer } from "./todoReducer";
 
 const initialState = [
-  {
-    id: new Date().getTime(),
-    description: "Recolectar la gema del alma",
-    done: false,
-  },
-  {
-    id: new Date().getTime() * 3,
-    description: "Recolectar la gema del infinito",
-    done: false,
-  },
+  // {
+  //   id: new Date().getTime(),
+  //   description: "Recolectar la gema del alma",
+  //   done: false,
+  // },
+  // {
+  //   id: new Date().getTime() * 3,
+  //   description: "Recolectar la gema del infinito",
+  //   done: false,
+  // },
 ];
+
+const init = () =>{
+  return JSON.parse(localStorage.getItem("todos")) || []
+}
 
 export const TodoApp = () => {
 
-  const [todos, dispatch] = useReducer(todoReducer, initialState);
+  const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+
+  useEffect(() =>{
+     localStorage.setItem("todos", JSON.stringify(todos))
+  },[todos])
 
 
    const handleNewTodo = (todo) =>{
